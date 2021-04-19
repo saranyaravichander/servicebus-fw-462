77880c4417ab39c0ef3a1dbc3e31e7a34f1702d1
saranyaravichander <saranyaravichander@live.com>
2021-04-19 09:51:54 -0400

    Init

---
+++ TestAzureConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace TestAzureConsole
+{
+    class Program
+    {
+        //private static MessagingFactory messagingFactory;
+
+        static string connectionString = "Endpoint=sb://<>.servicebus.windows.net/;SharedAccessKeyName=<>;SharedAccessKey=<>;EntityPath=<>";
+
+        static void Main(string[] args)
+        {
+            var client = QueueClient.CreateFromConnectionString(connectionString);
+
+            string message = "Send Message1";
+
+            while (true)
+            {
+                client.Send(new BrokeredMessage(message));
+                Console.WriteLine("Sent: " + message.ToString());
+                Console.WriteLine("Enter a message to send");
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ TestAzureConsoleReciever/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+//using Microsoft.ServiceBus.Messaging;
+
+namespace TestAzureConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueConnectionString = "Endpoint=sb://<>.servicebus.windows.net/;SharedAccessKeyName=<>;SharedAccessKey=<>;EntityPath=<>";
+
+            Action<BrokeredMessage> onMessage = (bm) => {
+                var message = bm.GetBody<string>();
+                Console.WriteLine(message.ToString());
+                bm.Complete();
+            };
+
+            var options = new OnMessageOptions()
+            {
+                AutoComplete = false
+            };
+
+            var client = QueueClient.CreateFromConnectionString(queueConnectionString);
+
+            client.OnMessage(onMessage, options);
+
+            Console.WriteLine("End of receiver program");
+            Console.ReadLine();
+
+        }
+    }
+}
